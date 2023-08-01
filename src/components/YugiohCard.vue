049afcbe0c4576d809fe8e631d544446b999a137
kooriookami <bingshuanglingluo@163.com>
2023-08-01 17:57:14 +0800

    update

--- src/components/YugiohCard.vue
+++ src/components/YugiohCard.vue
@@ -115,7 +115,7 @@
     cardLeaf.value = new Card({
       view: card.value,
       data: form.data,
-      resourcePath: '/src/assets/yugioh-card',
+      resourcePath: process.env.NODE_ENV === 'production' ? 'https://raw.githubusercontent.com/kooriookami/yugioh-card/master/src/assets/yugioh-card' : 'src/assets/yugioh-card',
     });
     jsonData.value = form.data;
   }
