9a971935f860d109338f221b669fb495f12aedc8
kooriookami <bingshuanglingluo@163.com>
2026-03-23 10:35:35 +0800

    chore(index, package.json, vite.config.js): 更新配置，优化依赖管理

--- vite.config.js
+++ vite.config.js
@@ -69,7 +69,6 @@ export default defineConfig(({ mode }) => {
       alias: {
         '@': path.resolve(__dirname, 'src'),
       },
-      conditions: ['browser'],
     },
     build: buildConfigMap[buildTarget],
   };
